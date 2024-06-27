263e87121b64a916acddf03be115324eb961f085
dianprsty <dprasmail@gmail.com>
2024-06-27 08:44:23 +0700

    priject setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center bg-blue-200">
-        <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold text-center text-blue-950">
-          React JS + Typecript + Tailwind CSS Project Starter
-        </h1>
-      </div>
+      <main className="flex items-center justify-center w-screen h-screen bg-custom-yelow">
+        <section className="w-full p-4 bg-white max-w-96 rounded-2xl">
+          <img
+            src="/images/illustration-article.svg"
+            alt=""
+            className="w-full rounded-2xl"
+          />
+          <h1 className="text-xl italic font-bold text-center sm:text-2xl md:text-4xl text-custom-black ">
+            React JS + Typecript +{" "}
+            <span className="">Tailwind CSS Project Starter</span>
+          </h1>
+        </section>
+      </main>
+      <footer className="fixed bottom-0 left-0 w-full p-4 bg-custom-yelow">
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          .
+          <br />
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </footer>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        custom: {
+          yelow: "hsl(47, 88%, 63%)",
+          white: "hsl(0, 0%, 100%)",
+          grey: "hsl(0, 0%, 50%)",
+          black: "hsl(0, 0%, 7%)",
+        },
+      },
+      fontFamily: {
+        figtree: ["Figtree", "san-serif"],
+      },
+    },
   },
   plugins: [],
 };
